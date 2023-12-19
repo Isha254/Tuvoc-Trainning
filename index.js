a6e1d5c5484bae6533ef109139d721fbf0d9fe1a
Isha Bhalodiya <137141666+Isha254@users.noreply.github.com>
2023-12-19 16:41:48 +0530

    Add files via upload
    Ch-1 a.variable, Data types

---
+++ index.js
@@ -0,0 +1,12 @@
+// --- a. Variables & Data Types---
+
+/* Task: Create a program that stores details about a person (name, age, hobbies)
+in appropriate data types. */
+
+
+const name = "Isha";
+let age = 20;
+let hobbies = ['reading', 'painting', 'dancing'];
+console.log(name, age, hobbies);
+
+
